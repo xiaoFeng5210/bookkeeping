e8e6be439745384e1c043c6eb5a7049ea99f97c8
Eadweard <804875728@qq.com>
2023-07-07 11:37:35 +0800

    user controler

--- app/controller/user.js
+++ app/controller/user.js
@@ -25,6 +25,28 @@ class UserController extends Controller {
       }
       return
     }
+
+    const defaultAvatar = 'http://s.yezgea02.com/1615973940679/WeChat77d6d2ac093e247c361f0b8a7aeb6c2a.png'
+    const result = await ctx.service.user.register({
+      username,
+      password,
+      signature: '世界和平',
+      avatar: defaultAvatar
+    })
+
+    if (result) {
+      ctx.body = {
+        code: 200,
+        msg: '注册成功',
+        data: null
+      }
+    } else {
+      ctx.body = {
+        code: 500,
+        msg: '注册失败',
+        data: null
+      }
+    }
   }
 }
 
