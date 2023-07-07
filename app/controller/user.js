0e0f3a59a272760ec409e4b82267022a2eb03b05
Eadweard <804875728@qq.com>
2023-07-07 11:21:02 +0800

    fix

--- app/controller/user.js
+++ app/controller/user.js
@@ -6,6 +6,14 @@ class UserController extends Controller {
   async register() {
     const {ctx} = this
     const {username, password} = ctx.request.body
+
+    if (!username || !password) {
+      ctx.body = {
+        code: 500,
+        msg: '用户名或密码不能为空',
+        data: null
+      }
+    }
   }
 }
 
